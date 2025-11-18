b06fa3e6d1c88f07f3fffcf1d9f18005a408c961
junhee han <1702o505@naver.com>
2025-11-18 09:49:22 +0900

    1118update: UI designer work

--- src/main/java/com/example/ex02/Ex01Application.java
+++ src/main/java/com/example/ex02/Ex01Application.java
@@ -12,3 +12,6 @@ public class Ex01Application {
 	}
 
 }
+
+
+
--- src/main/java/com/example/ex02/festival/entity/FavoriteEntity.java
+++ src/main/java/com/example/ex02/festival/entity/FavoriteEntity.java
@@ -2,12 +2,26 @@
 //FavoriteEntity
 package com.example.ex02.festival.entity;
 
-import com.example.ex02.festival.entity.FestivalEntity;
-import com.example.ex02.member.entity.MemberEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
+
+import com.example.ex02.member.entity.MemberEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(
